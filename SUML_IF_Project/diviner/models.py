9db109d5e5061e4bfe738e4420a4e8169c212cb6
Tomasz Iwanowski <t.iwanowski96@gmail.com>
2022-01-11 18:25:23 +0100

    Initial creation of models

--- SUML_IF_Project/diviner/models.py
+++ SUML_IF_Project/diviner/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Diabetes(models.Model):
+    sick = models.BooleanField(null=True)
+    HIGH_BP = [
+        (1, 'HighBloodPreasure'),
+        (0, 'LowBloodPreasure')
+    ]
+    high_blood_preasure = models.CharField(
+        choices=HIGH_BP,
+        null=True,
+        max_length=1
+    )
+    CHOLESTEROL = [
+        (1, 'HighCholesterol'),
+        (0, 'LowCholesterol')
+    ]
+    cholesterol = models.CharField(
+        choices=CHOLESTEROL,
+        null=True,
+        max_length=1
+    )
+
+    def __str__(self):
+        return self.sick
