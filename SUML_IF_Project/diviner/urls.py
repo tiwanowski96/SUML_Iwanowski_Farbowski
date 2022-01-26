b306433271bf4fba10998a3fc2fb3b7bfb5c7ee5
Tomasz Iwanowski <t.iwanowski96@gmail.com>
2022-01-26 19:37:21 +0100

    Add plot to home view

--- SUML_IF_Project/diviner/urls.py
+++ SUML_IF_Project/diviner/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.HomeView.as_view(), name='home'),
     path('upload_form/', views.upload_form, name='upload_form'),
     path('add_diabetes/', views.DiabetesAddView.as_view(), name='add_diabetes'),
     path('diabetic/<int:id>/', views.DiabetesView.as_view(), name='diabetic'),
--- SUML_IF_Project/diviner/views/home.py
+++ SUML_IF_Project/diviner/views/home.py
@@ -1,5 +1,32 @@
+import base64
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
 from django.shortcuts import render
+from django.views.generic import View
+from io import BytesIO
 
+class HomeView(View):
 
-def home(request):
-    return render(request, 'diviner/home.html')
+    def get(self, request):        
+        
+        plot = self.plot(self)
+        return render(request, 'diviner/home.html')
+
+    def plot(self, request):
+
+        all_entries = pd.read_csv('diabetes.csv')
+        all_entries.columns
+        cols = ["sick","sex","age","high_blood_preasure","cholesterol","bmi","smoker","stroke","heart_disease_or_attack",
+                "physical_activity","heavy_alcohol_consumption","healthcare","general_health_feeling","mental_health_feeling",
+                "physical_health_feeling","education"]
+        data = all_entries[cols].copy()
+        print('data: %s' % data)
+        data.isnull().any()
+        sns.set_style('whitegrid')
+        plot = sns.countplot(x='sick', data=data)
+        fig = plot.get_figure()
+        plot_png = fig.savefig("SUML_IF_Project/diviner/static/diviner/plot.png") 
+
+        return plot_png
